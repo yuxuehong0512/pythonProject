44848414dd7ec75f4b7b435504853e297cc24846
yueryuer <2808723214@qq.com>
2020-09-22 11:22:01 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycha
+# """用python设计第一个游戏"""
+# temp = input("""不妨猜一下小甲鱼现在心里面想的是哪个数字""")
+# guess = int(temp)
+# if guess == 8:
+#     print("你是小甲鱼心里面的蛔虫嘛？！")
+#     print("猜中了也没有奖励")
+# else:
+    # print("猜错了，小甲鱼现在心里面想的是8")
+# print("游戏结束了，不玩了")
+# 幸运苏 = 588
+# print(幸运苏)
+# # x = 3
+# # y = 5
+# # print(x,y)
+# # x,y = y,x
+# # print(x,y)
+print("I love python.\nI LOVE HH")
+# first = 520
+# second = '520'
+# first = second
+
+print('I love fishC\n')
+
+
